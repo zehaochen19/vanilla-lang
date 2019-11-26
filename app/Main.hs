54eaca7e860a9d761d4a92ddd63a108aabed4831
zehaochen19 <zehaochen19@gmail.com>
2019-11-26 12:37:21 -0800

    add examples of typechecking

--- app/Main.hs
+++ app/Main.hs
@@ -7,4 +7,10 @@ import           Static.TypeCheck               ( typecheck )
 main :: IO ()
 main = do
   print $ typecheck id'
+  print $ typecheck idId
+  print $ typecheck idIdAnno
   print $ typecheck idUnit
+  print $ typecheck nestedId
+  print $ typecheck nestedIdUnit
+  print $ typecheck nestedIdId
+  print $ typecheck nestedIdIdUnit
--- src/Example.hs
+++ src/Example.hs
@@ -9,8 +9,31 @@ import           Static.Type                    ( Type(..) )
 
 
 id' :: Expr
-id' = EAnno (ELam "x" (EVar "x")) (TAll "A" (TArr (TVar "A") (TVar "A")))
+id' = EAnno (ELam "x" (EVar "x")) (TAll "B" (TArr (TVar "B") (TVar "B")))
 
 idUnit :: Expr
 idUnit = EApp id' EUnit
 
+idId :: Expr
+idId = EApp id' id'
+
+idIdAnno :: Expr
+idIdAnno = EAnno idId (TAll "A" (TArr (TVar "A") (TVar "A")))
+
+
+nestedId :: Expr
+nestedId = EAnno
+  (ELam "f" (ELam "x" (EApp (EVar "f") (EVar "x"))))
+  (TAll
+    "A"
+    (TArr (TAll "A" (TArr (TVar "A") (TVar "A"))) (TArr (TVar "A") (TVar "A")))
+  )
+
+nestedIdUnit :: Expr
+nestedIdUnit = EApp (EApp nestedId id') EUnit
+
+nestedIdId :: Expr
+nestedIdId = EApp (EApp nestedId id') id'
+
+nestedIdIdUnit :: Expr
+nestedIdIdUnit = EApp (EApp (EApp nestedId id') id') EUnit
