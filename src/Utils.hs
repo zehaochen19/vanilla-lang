3e990f9558e34cba9153ec87355161de78d06efb
zehaochen19 <zehaochen19@gmail.com>
2019-12-04 12:57:16 -0800

    add utility functions to manipulate Nats

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,5 +1,7 @@
 module Utils where
 
+import Syntax.Expr
+
 freshVarStream :: [String]
 freshVarStream = do
   digit <- nums
@@ -8,3 +10,12 @@ freshVarStream = do
   where
     letters = ['a' .. 'z']
     nums = [0 ..]
+
+natToInt :: Expr -> Integer
+natToInt EZero = 0
+natToInt (ESucc n) = 1 + natToInt n
+natToInt _ = 0
+
+intToNat :: Integer -> Expr
+intToNat n | n <= 0 = EZero
+intToNat n = ESucc . intToNat $ n - 1
