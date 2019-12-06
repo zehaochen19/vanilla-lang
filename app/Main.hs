4d0149f22b7ea74340f3545f83103d0eca917eab
zehaochen19 <zehaochen19@gmail.com>
2019-12-05 22:46:28 -0800

    add main function

--- app/Main.hs
+++ app/Main.hs
@@ -1,47 +1,24 @@
 module Main where
 
-import Dynamic.Step (eval)
-import Static.TypeCheck (typecheck)
-import SystemF.Program
+import qualified Data.Text.IO as TIO
+import System.Environment
+import System.Exit
+import SystemF
 
 main :: IO ()
 main = do
-  putStrLn "id'"
-  print $ typecheck id'
-  print $ eval id'
-  putStrLn "idIdAnno"
-  print $ typecheck idIdAnno
-  print $ eval idIdAnno
-  putStrLn "idUnit"
-  print $ typecheck idUnit
-  print $ eval idUnit
-  putStrLn "idUnit'"
-  print $ typecheck idUnit'
-  print $ eval idUnit'
-  putStrLn "nestedId"
-  print $ typecheck nestedId
-  print $ eval nestedId
-  putStrLn "nestedIdId"
-  print $ typecheck nestedIdId
-  print $ eval nestedIdId
-  putStrLn "nestedIdUnit"
-  print $ typecheck nestedIdUnit
-  print $ eval nestedIdUnit
-  putStrLn "nestedIdIdUnit"
-  print $ typecheck nestedIdIdUnit
-  print $ eval nestedIdIdUnit
-  putStrLn "letIdUnit"
-  print $ typecheck letIdUnit
-  print $ eval letIdUnit
-  putStrLn "letNestedIdId"
-  print $ typecheck letNestedIdId
-  print $ eval letNestedIdId
-  putStrLn "letNestedIdUnit"
-  print $ typecheck letNestedIdUnit
-  print $ eval letNestedIdUnit
-  putStrLn "mistypedLetNestedUnit"
-  print $ typecheck illtypedLetNestedUnit
-  print $ eval illtypedLetNestedUnit
-  putStrLn "lambdaIdIdUnit"
-  print $ typecheck lambdaIdIdUnit
-  print $ eval lambdaIdIdUnit
+  args <- getArgs
+  case args of
+    (src : _) -> do
+      prog <- TIO.readFile src
+      case runSystemF src prog of
+        Left error -> putStrLn error
+        Right (expr, ty) -> do
+          putStrLn "Type:"
+          print ty
+          putStrLn ""
+          putStrLn "Result:"
+          print expr
+    _ -> do
+      putStrLn "Usage: systemf src.sf"
+      exitFailure
--- src/SystemF.hs
+++ src/SystemF.hs
@@ -4,7 +4,9 @@
 
 module SystemF where
 
+import Data.Text (Text)
 import Dynamic.Step (eval)
+import Parser (runProgramP)
 import Polysemy
 import Polysemy.Error
 import Polysemy.State
@@ -14,10 +16,20 @@ import Syntax.Expr
 import Syntax.Type
 
 -- | Given an expression, first typecheck it and then evaluate it
-interpret :: Member (Error String) r => Expr -> Sem r (Expr, Type)
-interpret expr = do
+interpretF :: Member (Error String) r => Expr -> Sem r (Expr, Type)
+interpretF expr = do
   (ty, ctx) <-
     evalState initCheckState $
       synthesize mempty expr
         `catch` (\e -> throw $ "Typecheck error:\n" ++ e)
   return (eval expr, applyCtx ctx ty)
+
+systemF :: Member (Error String) r => FilePath -> Text -> Sem r (Expr, Type)
+systemF src prog = do
+  expr <-
+    fromEither (runProgramP src prog)
+      `catch` (\e -> throw $ "Parse error:\n" ++ e)
+  interpretF expr
+
+runSystemF :: FilePath -> Text -> Either String (Expr, Type)
+runSystemF path src = run . runError $ systemF path src
