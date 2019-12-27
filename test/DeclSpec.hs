02e7fc18066eb9e886b91db2073d75abecb62397
zehaochen19 <zehaochen19@gmail.com>
2019-12-27 12:24:54 -0800

    add test case for checking ADT duplication

--- test/DeclSpec.hs
+++ test/DeclSpec.hs
@@ -3,18 +3,22 @@
 module DeclSpec where
 
 import           Data.Map                      as M
+import           Data.Either                    ( isLeft )
 import           Test.Hspec
 import           Vanilla.Examples
 import           Vanilla.Static.TypeCheck.DeclCheck
 
 
 
-declSpec = describe "declMap" $ do
+declSpec = describe "declMap should" $ do
   it "map List declaration to a map" $ do
     let Right declMap = runCheckDataTypes [listDec]
     M.lookup "List" declMap `shouldBe` Just listDec
   it "map Nat declaration to a map" $ do
     let Right declMap = runCheckDataTypes [natDec]
     M.lookup "Nat" declMap `shouldBe` Just natDec
+  it "reject duplicate declarations"
+    $               runCheckDataTypes [natDec, natDec]
+    `shouldSatisfy` isLeft
 
 
